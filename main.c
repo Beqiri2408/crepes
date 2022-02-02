b95f46e7e132aea96185a395d4bf3d017c65d33b
Beqiri2408 <musli.beqiri@cpnv.ch>
2022-02-02 16:58:16 +0100

    Ajout des constantes

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define FLOUR_PER_PERSONS 125
+#define MILK_PER_PERSONS 0.25
+#define EGGS_PER_PERSONS 1.5
+#define SALT_PER_PERSONS 0.5
 int main() {
     printf("Hello, World!\n");
     return 0;
